84a706ab773c2d402c751d4ffa8eb4566bff4fbf
Nyfaria DragonMoose <kinaria3324@gmail.com>
2024-02-24 18:18:11 -0600

    forgot new book command

--- src/main/java/net/swimmingtuna/lotm/init/CommandInit.java
+++ src/main/java/net/swimmingtuna/lotm/init/CommandInit.java
@@ -1,9 +1,12 @@
 package net.swimmingtuna.lotm.init;
 
+import net.minecraft.commands.Commands;
 import net.minecraft.commands.synchronization.ArgumentTypeInfo;
 import net.minecraft.commands.synchronization.ArgumentTypeInfos;
 import net.minecraft.commands.synchronization.SingletonArgumentInfo;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -13,6 +16,7 @@ import net.swimmingtuna.lotm.LOTM;
 import net.swimmingtuna.lotm.commands.AbilitiesCommand;
 import net.swimmingtuna.lotm.commands.BeyonderClassArgument;
 import net.swimmingtuna.lotm.commands.BeyonderCommand;
+import vazkii.patchouli.common.item.PatchouliItems;
 
 
 public class CommandInit {
@@ -24,5 +28,18 @@ public class CommandInit {
     public static void onCommandRegistration(RegisterCommandsEvent event) {
         BeyonderCommand.register(event.getBuildContext(),event.getDispatcher());
         AbilitiesCommand.register(event.getDispatcher());
+
+        event.getDispatcher().register(
+                Commands.literal("lotmbook").executes(
+                        (context) -> {
+                            ItemStack book = new ItemStack(PatchouliItems.BOOK);
+                            CompoundTag tag = new CompoundTag();
+                            tag.putString("patchouli:book", "lotm:lotmbook");
+                            book.setTag(tag);
+                            context.getSource().getPlayerOrException().getInventory().add(book);
+                            return 1;
+                        }
+                )
+        );
     }
 }
